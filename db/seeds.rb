4f01afc1b6e4ed41b71949305ef60f172fa9bd31
Ellen Cornelius <ecorneli@iwu.edu>
2018-01-25 23:12:08 -0700

    Add pry to gems. Remove validations from Merchant model. Ran rakedb:seed to confirm population of database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,13 @@
 require 'csv'
+require_relative '../app/models/merchant.rb'
+
 class Seeds
   def self.from_csv(file_path)
     data = CSV.open file_path, headers: true, header_converters: :symbol, converters: :numeric
     data.each do |row|
-      row
+      Merchant.create(row.to_hash)
     end
   end
 end
 
-Seeds.from_csv('../data/merchants.csv')
+Seeds.from_csv('data/merchants.csv')
