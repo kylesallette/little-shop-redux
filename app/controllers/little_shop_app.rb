ae4f7bfece62a376dbb7bf03550b42cca3a6472d
kylesallette <kylesallette@gmail.com>
2018-01-27 08:56:48 -0700

    adding edit search for merchant page

--- app/controllers/little_shop_app.rb
+++ app/controllers/little_shop_app.rb
@@ -12,6 +12,11 @@ class LittleShopApp < Sinatra::Base
     erb :"merchants/show_input"
   end
 
+  get "/merchants/edit_input" do
+    @merchants = Merchant.all
+    erb :"merchants/edit_input"
+  end
+
   get "/merchants" do
     @merchants = Merchant.all
     erb :"merchants/index"
@@ -26,13 +31,23 @@ class LittleShopApp < Sinatra::Base
     erb :"merchants/show"
   end
 
+  get "/merchants/:id" do
+    @merchant = Merchant.find(params[:id])
+    erb :"merchants/show"
+  end
+
   post "/merchants" do
     merchant = Merchant.new(params[:merchant])
     merchant.save
     redirect "/merchants"
   end
 
-  get "/merchants/:id/edit" do
+  get "/merchants/edit/" do
+    @merchant = Merchant.find(params[:id])
+    erb :"merchants/edit"
+  end
+
+  get "/merchants/edit/:id" do
     @merchant = Merchant.find(params[:id])
     erb :"merchants/edit"
   end
