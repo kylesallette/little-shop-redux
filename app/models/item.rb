492024ce463f961fe91495476e684180b80c37c4
slimecog <wlcjohnson@gmail.com>
2018-01-28 14:25:40 -0700

    add item validation tests

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ActiveRecord::Base
   validates_presence_of :name,
                         :description,
-                        :unit_price
+                        :unit_price,
                         :merchant_id
 end
