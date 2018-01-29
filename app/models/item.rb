79d4c6c94d5d39ca905c64addea2a9229ee1da5e
slimecog <wlcjohnson@gmail.com>
2018-01-29 15:46:40 -0700

    add new csv for items

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,22 @@ class Item < ActiveRecord::Base
                         :description,
                         :unit_price,
                         :merchant_id
+  belongs_to            :merchants
+  belongs_to            :categories
+
+  def self.total
+    Item.all.count
+  end
+
+  def self.average_price
+    (average(:unit_price).to_f / 100).round(2)
+  end
+
+  def self.newest_item
+    Item.last
+  end
+
+  def self.oldest_item
+    Item.first
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180128205236) do
+ActiveRecord::Schema.define(version: 20180129211346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20180128205236) do
     t.integer "merchant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
   end
 
   create_table "merchants", force: :cascade do |t|
