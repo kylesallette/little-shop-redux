76ae02660bf72529f13c406cd60af4702621a390
Cameron M Johnson <wlcjohnson@gmail.com>
2018-01-29 20:50:13 -0700

    Update 20180127010429_create_categories.rb

--- db/migrate/20180127010429_create_categories.rb
+++ db/migrate/20180127010429_create_categories.rb
@@ -2,7 +2,7 @@ class CreateCategories < ActiveRecord::Migration[5.1]
   def change
     create_table :categories do |t|
       t.string :name
-      t.string :image_id
+#       t.string :image_id
     end
   end
 end
