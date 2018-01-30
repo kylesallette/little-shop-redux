d1d52bcd9669689490ff11b2bae2de288b55ee47
Cameron M Johnson <wlcjohnson@gmail.com>
2018-01-29 20:48:50 -0700

    Update category.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ActiveRecord::Base
   validates_presence_of :name
   has_many              :items
-  before_validation     :default_image
+#   before_validation     :default_image
 
   # SOME IDEAS ON HOW TO FIND Category with most expensive item && Category with least expensive item.
   #
@@ -15,11 +15,11 @@ class Category < ActiveRecord::Base
   #   items.find_by(:unit_price lowest_price).category
   # end
 
-  def default_image
-    if self.id == 1
-      self.image = "http://www.ptahai.com/wp-content/uploads/2016/06/Best-Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg"
-    end
-  end
+#   def default_image
+#     if self.id == 1
+#       self.image = "http://www.ptahai.com/wp-content/uploads/2016/06/Best-Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg"
+#     end
+#   end
 end
 
 
