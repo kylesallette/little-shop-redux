328513a3ab948c6916ac4ce931a562befa4dc6d1
slimecog <wlcjohnson@gmail.com>
2018-01-30 17:25:23 -0700

    add merchant analytics

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -9,4 +9,19 @@ class Merchant < ActiveRecord::Base
     .order("items_count_by_merchant_id DESC")
     .first
   end
+
+  def self.highest_priced_item
+    highest_price = Item.maximum(:unit_price)
+    Item.find_by(unit_price: highest_price).merchant
+  end
+
+  def item_count
+    self.items.count
+  end
+
+  def total_item_price
+    self.items.map do |item|
+      item.unit_price
+    end.sum
+  end
 end
